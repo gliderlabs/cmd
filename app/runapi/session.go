477518500a1269f6d016714717cc9b0483e95711
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2017-05-14 20:21:23 -0400

    Implement CGI when using web-based cmd. Inject some useful ENV vars no matter what the context.

--- app/runapi/session.go
+++ app/runapi/session.go
@@ -5,9 +5,16 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 
+	"github.com/gliderlabs/cmd/lib/release"
 	"github.com/gliderlabs/ssh"
+)
+
+const (
+	ServerProtocol   = "HTTP/1.1"
+	GatewayInterface = "CGI/1.1"
 )
 
 type httpSession struct {
@@ -17,6 +24,7 @@ type httpSession struct {
 	token       string
 	isWebSocket bool
 	ctx         context.Context
+	cmd         []string
 }
 
 func (sess *httpSession) Write(p []byte) (n int, err error) {
@@ -44,13 +52,34 @@ func (sess *httpSession) RemoteAddr() net.Addr {
 	return &net.IPAddr{IP: net.ParseIP(sess.req.RemoteAddr)}
 }
 func (sess *httpSession) Environ() []string {
-	// TODO
-	return []string{}
+	if sess.req == nil {
+		return nil
+	}
+	sh := strings.Split(sess.req.Host, ":")
+	port := ""
+	if len(sh) > 1 {
+		port = sh[1]
+	}
+	return []string{
+		"SERVER_NAME=" + release.Hostname(),
+		"SERVER_PROTOCOL=" + ServerProtocol,
+		"HTTP_HOST=" + release.Hostname(),
+		"GATEWAY_INTERFACE=" + GatewayInterface,
+		"REQUEST_METHOD=" + sess.req.Method,
+		"QUERY_STRING=" + sess.req.URL.RawQuery,
+		"REQUEST_URI=" + sess.req.URL.RequestURI(),
+		"PATH_INFO=" + sess.req.URL.Path,
+		"SCRIPT_NAME=" + sess.Command()[0],
+		"SERVER_PORT=" + port,
+		"CONTENT_TYPE=" + sess.req.Header.Get("Content-Type"),
+		"CONTENT_LENGTH=" + strconv.Itoa(int(sess.req.ContentLength)),
+	}
 }
+
 func (sess *httpSession) Command() []string {
-	// TODO
-	return []string{}
+	return append([]string(nil), sess.cmd...)
 }
+
 func (sess *httpSession) Pty() (ssh.Pty, <-chan ssh.Window, bool) {
 	var winch chan ssh.Window
 	//if sess.isWebSocket {
